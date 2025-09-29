dd8100046f2d8ddc65929f8cb58c2f46d54d42f5
Nikola-Kraljevic <nikola.kraljevic1c@gmail.com>
2025-09-29 13:11:48 +0200

    Add format keymap

--- lua/nikola/remap.lua
+++ lua/nikola/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -66,10 +67,6 @@ vim.keymap.set(
     "<leader>el",
     "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
 )
-
-vim.keymap.set("n", "<leader>ca", function()
-    require("cellular-automaton").start_animation("make_it_rain")
-end)
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
